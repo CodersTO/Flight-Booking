cf73e5adedb80c61f9fa06fe2b95337175108892
Kishore Narang <narangkishore98@gmail.com>
2020-08-24 12:17:53 -0400

    Initial Commit

---
+++ Flight Booking WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Flight Booking WatchKit Extension
+//
+//  Created by Kishore Narang on 2020-08-24.
+//  Copyright © 2020 Kishore Narang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
